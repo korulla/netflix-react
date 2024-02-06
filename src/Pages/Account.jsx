f29b3ade4c74c9ae22cfcfc412abff5c404f1436
Ben Korulla Thomas <benkorulla@gmail.com>
2024-02-07 05:19:46 +0530

    search started

--- src/Pages/Account.jsx
+++ src/Pages/Account.jsx
@@ -7,7 +7,7 @@ const Account = () => {
       <div className='w-full text-white'>
         <img
           className='w-full h-[400px] object-cover'
-          src="public/Images/signup-banner.jpg" alt="banner" />
+          src="Images/signup-banner.jpg" alt="banner" />
         <div className='bg-black/60 fixed top-0 left-0 w-full h-[550px] '></div>
         <div className='absolute top-[20%] p-4 md:p-8'>
           <h1 className='text-3xl md:text-5xl font-bold'>My shows</h1>
